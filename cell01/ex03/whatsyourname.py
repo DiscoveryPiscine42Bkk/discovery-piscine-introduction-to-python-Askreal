ef4823bb85d2ed49c632b99fb5bc7db471725db0
LackSweet <86765808+Askreal@users.noreply.github.com>
2025-06-23 06:19:17 +0000

    Add new file

---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,8 @@
+# Hey, what's your first name? : Wil
+# And your last name? : 42
+# Well, pleased to meet you, Wil 42.
+
+first_name = str(input("Hey, what's your first name? : "))
+last_name = str(input("And your last name? : "))
+
+print(f"Well, pleased to meet you, {first_name} {last_name}.")
