4db8dcce212cdf3f391c881cbf010efded4f618b
LackSweet <86765808+Askreal@users.noreply.github.com>
2025-06-23 08:27:19 +0000

    Add new file

---
+++ cell04/ex01/age.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Please tell me your age: 15
+# You are currently 15 years old.
+# In 10 years, you'll be 25 years old.
+# In 20 years, you'll be 35 years old.
+# In 30 years, you'll be 45 years old.
+
+age = int(input("Please tell me your age: "))
+print(f"You are currently {age} years old.")
+print(f"In 10 years, you'll be {age + 10} years old.")
+print(f"In 20 years, you'll be {age + 20} years old.")
+print(f"In 30 years, you'll be {age + 30} years old.")
