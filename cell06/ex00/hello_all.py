e94712eb3677157aa58f5aca6bca6b80c242ba4e
LackSweet <86765808+Askreal@users.noreply.github.com>
2025-06-24 06:07:46 +0000

    fix : use function

--- cell06/ex00/hello_all.py
+++ cell06/ex00/hello_all.py
@@ -1,13 +1,6 @@
 #!/usr/bin/env python3
-# class halo:
-#     def hello():
-#         print(f'Hello, everyone!')
-    
+
 def hello():
     print(f'Hello, everyone!')
     
 hello()
-# halo.hello()
-
-# I'm not sure which one is correct as I understand the frist one call method but the second one is look similar with example
-# SO I'll leave it like this lol
