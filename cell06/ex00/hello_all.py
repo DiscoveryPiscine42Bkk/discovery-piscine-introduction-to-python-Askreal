ce41570ab81b7f353f358509acb62e1a75b2ca2f
LackSweet <86765808+Askreal@users.noreply.github.com>
2025-06-24 06:03:49 +0000

    Add new file

---
+++ cell06/ex00/hello_all.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# class halo:
+#     def hello():
+#         print(f'Hello, everyone!')
+    
+def hello():
+    print(f'Hello, everyone!')
+    
+hello()
+# halo.hello()
+
+# I'm not sure which one is correct as I understand the frist one call method but the second one is look similar with example
+# SO I'll leave it like this lol
