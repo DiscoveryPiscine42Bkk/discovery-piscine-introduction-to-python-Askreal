ebb4a2339aef1f483909125185968134641522c3
LackSweet <86765808+Askreal@users.noreply.github.com>
2025-06-24 04:29:22 +0000

    Add new file

---
+++ cell05/ex12/string_are_arrays.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import sys
+import re
+
+if len(sys.argv or 'z' not in sys.argv[1]) != 2:
+    print(f'none')
+else :
+    text = sys.argv[1]
+    print(f'z'*len(re.findall('z' , text)))    
